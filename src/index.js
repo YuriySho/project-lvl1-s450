54274f38717f8bd91e2d4ef8e9ee9fbda89d49c9
Yuriy Shoshin <yuriy.shoshin.87@mail.ru>
2019-02-26 11:07:16 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const actual = readlineSync.question('May I have your name?: ');
-
 export default () => {
+    console.log('Welcome to the Brain Games!');
+    const actual = readlineSync.question('May I have your name?: ');
     console.log(`Hello, ${actual}`);
-};
+}
